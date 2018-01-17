dc2eab16e78dd25d5525125009374cc5f35a3131
Matt Hough <matt@hough.tv>
2018-01-18 07:12:11 +1000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const app = "I don't do much.";
+const token = "TOKEN_HERE"  
+
+function getRepos() {
+  fetch("https://api.github.com/user/repos", {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
+}
